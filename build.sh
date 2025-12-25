d4b0107ed62e80687daec5b0d8dff4e8fa963316
Bokhtear Md Abid <abidmuhammad206@gmail.com>
2025-12-26 03:22:11 +0600

    update

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Exit immediately if any command fails
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
